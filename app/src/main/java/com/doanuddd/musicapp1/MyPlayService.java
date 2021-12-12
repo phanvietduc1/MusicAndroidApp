81aa9484df78956d6c5204acc5b244495a683d17
phanvietduc1 <43225338+phanvietduc1@users.noreply.github.com>
2021-12-12 23:24:01 +0700

    playservice

--- app/src/main/java/com/doanuddd/musicapp1/MyPlayService.java
+++ app/src/main/java/com/doanuddd/musicapp1/MyPlayService.java
@@ -4,10 +4,18 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.widget.Toast;
 
-public class MyPlayService extends Service implements MediaPlayer.OnCompletionListener.MediaPla {
+public class MyPlayService extends Service implements MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener,
+    MediaPlayer.OnErrorListener, MediaPlayer.OnSeekCompleteListener, MediaPlayer.OnInfoListener,
+        MediaPlayer.OnBufferingUpdateListener {
+
+    private MediaPlayer mediaPlayer;
+    private String linkSong;
+
     public MyPlayService() {
     }
+
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -18,15 +26,89 @@ public class MyPlayService extends Service implements MediaPlayer.OnCompletionLi
     @Override
     public void onDestroy() {
         super.onDestroy();
+
+        if (mediaPlayer != null){
+            if (mediaPlayer.isPlaying()){
+                mediaPlayer.stop();
+            }
+
+            mediaPlayer.release();
+        }
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        linkSong = intent.getStringExtra("song");
+        mediaPlayer.reset();
+
+        if (!mediaPlayer.isPlaying()){
+            try{
+                mediaPlayer.setDataSource(linkSong);
+                mediaPlayer.prepareAsync();
+            }catch (Exception e){
+                Toast.makeText(this, "Error" + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+        return START_STICKY;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setOnCompletionListener(this);
+        mediaPlayer.setOnErrorListener(this);
+        mediaPlayer.setOnBufferingUpdateListener(this);
+        mediaPlayer.setOnSeekCompleteListener(this);
+        mediaPlayer.setOnInfoListener(this);
+        mediaPlayer.setOnPreparedListener(this);
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+        if (mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+        }
+
+        stopSelf();
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mediaPlayer, int i) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+        switch (i){
+            case MediaPlayer.MEDIA_ERROR_UNKNOWN:
+                Toast.makeText(this, "MEDIA_ERROR_UNKNOWN", Toast.LENGTH_SHORT).show();
+                break;
+            case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
+                Toast.makeText(this, "MEDIA_ERROR_SERVER_DIED", Toast.LENGTH_SHORT).show();
+                break;
+            case MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK:
+                Toast.makeText(this, "MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInfo(MediaPlayer mediaPlayer, int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+
+    }
+
+    @Override
+    public void onSeekComplete(MediaPlayer mediaPlayer) {
+
     }
 }
